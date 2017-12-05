6d237792acad9467796187ecaac9e3db2bdbd959
JKAMILO <camilomeneses1996@hotmail.com>
2017-12-04 19:04:58 -0500

    cambios en los css agregado asu carpeta y la carpet java scrit con el menu mobile

---
+++ javascript/menuMobile.js
@@ -0,0 +1,10 @@
+
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width
+        = '250px';
+}
+function cloeSlideMenu() {
+    document.getElementById('side-menu').style.width
+        = '0';
+
+}
