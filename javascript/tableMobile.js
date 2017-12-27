c9e9426d9adf6ff20575f96350d030f8ad7b6828
erick <erickarmandogarcia@hotmail.com>
2017-12-27 18:16:45 -0500

    Implement functionality to hide all rows within the table, also detects if we are in mobile or not

--- javascript/tableMobile.js
+++ javascript/tableMobile.js
@@ -1,28 +1,33 @@
+//detect if we are in mobile
+function isMobile() {
+	if (window.innerWidth <= 480) {
+		return true;
+	}
+	else {
+		false;
+	}
+}
+
 //this function hide all rows in a column
 function hideRows() {
-  var element = document.getElementsByClassName("column-number-0");
-  var children = element[0]["children"];
-
-  for (var i = 0; i < children.length; i++) {
-    if (children[i].className == "table-list-row") {
-      children[i]["style"]["display"] = "none";
-    }
-  }
+	var tableRows = document.getElementsByClassName("table-list-row");
+	for (var i = 0; i < tableRows.length; i++) {
+		tableRows[i]["style"]["display"] = "none";
+	}
 }
 //when the page has been loaded, this function is called
-hideRows();
+if (isMobile()) hideRows();
 
 /*This function is called when the user clicks a column */
 function ShowRows(clickedElement) {
-  var element = document.getElementsByClassName("column-number-0");
-  var children = element[0]["children"];
+	var children = clickedElement["children"];
 
-  for (var i = 0; i < children.length; i++) {
-    if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "none") {
-      children[i]["style"]["display"] = "block";
-    }
-    else if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "block") {
-      children[i]["style"]["display"] = "none";
-    }
-  }
+	for (var i = 0; i < children.length; i++) {
+		if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "none") {
+			children[i]["style"]["display"] = "flex";
+		}
+		else if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "flex") {
+			children[i]["style"]["display"] = "none";
+		}
+	}
 }
