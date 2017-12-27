0208eba6cb34cf11568c504be7423eb78c6399da
erick <erickarmandogarcia@hotmail.com>
2017-12-27 18:23:20 -0500

    Fix indentation

--- javascript/tableMobile.js
+++ javascript/tableMobile.js
@@ -1,33 +1,33 @@
 //detect if we are in mobile
 function isMobile() {
-	if (window.innerWidth <= 480) {
-		return true;
-	}
-	else {
-		false;
-	}
+  if (window.innerWidth <= 480) {
+    return true;
+  }
+  else {
+    false;
+  }
 }
 
 //this function hide all rows in a column
 function hideRows() {
-	var tableRows = document.getElementsByClassName("table-list-row");
-	for (var i = 0; i < tableRows.length; i++) {
-		tableRows[i]["style"]["display"] = "none";
-	}
+  var tableRows = document.getElementsByClassName("table-list-row");
+  for (var i = 0; i < tableRows.length; i++) {
+    tableRows[i]["style"]["display"] = "none";
+  }
 }
 //when the page has been loaded, this function is called
 if (isMobile()) hideRows();
 
 /*This function is called when the user clicks a column */
 function ShowRows(clickedElement) {
-	var children = clickedElement["children"];
+  var children = clickedElement["children"];
 
-	for (var i = 0; i < children.length; i++) {
-		if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "none") {
-			children[i]["style"]["display"] = "flex";
-		}
-		else if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "flex") {
-			children[i]["style"]["display"] = "none";
-		}
-	}
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "none") {
+      children[i]["style"]["display"] = "flex";
+    }
+    else if (children[i].className == "table-list-row" && children[i]["style"]["display"] == "flex") {
+      children[i]["style"]["display"] = "none";
+    }
+  }
 }
