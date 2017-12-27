6a293155351642b775b2ac29bf968684ffe7311c
erick <erickarmandogarcia@hotmail.com>
2017-12-26 21:00:12 -0500

    Rename pruebas folder

---
+++ archivosDePrueba/javascript/menuMobile.js
@@ -0,0 +1,9 @@
+
+function openSlideMenu() {
+	document.getElementById('side-menu').style.width = '210px';
+	document.getElementById('mobile-green-container').style.display = 'flex';
+}
+function cloeSlideMenu() {
+	document.getElementById('side-menu').style.width = '0';
+	document.getElementById('mobile-green-container').style.display = 'none';
+}
